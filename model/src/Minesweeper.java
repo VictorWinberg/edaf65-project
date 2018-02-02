a34c7f7d3a04be20070df14a179130ff332bf988
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2018-02-02 16:15:23 +0100

    test

--- model/src/Minesweeper.java
+++ model/src/Minesweeper.java
@@ -1,15 +1,18 @@
+import javax.swing.text.html.MinimalHTMLWriter;
+
 public class Minesweeper {
     private RealBoard board;
 
     public Minesweeper() {
-        board = new RealBoard(5);
+        board = new RealBoard(9);
+        board.placeRandomBombs(10);
     }
 
     public Board pick(int x, int y) {
         if (board.isBomb(x, y)) {
             return new LoserBoard(x,y);
         }
-        board.turnUp(x,y);
+        board.pick(x,y);
         return board.makeHiddenBoard();
     }
 
@@ -20,5 +23,9 @@ public class Minesweeper {
 
     public Board getInitialBoard(){
         return board.makeHiddenBoard();
+    }
+
+    public static void main(String[] args){
+        Minesweeper m = new Minesweeper();
     }
 }
