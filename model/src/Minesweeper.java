a33a747b9def17aad4ca133576ac901e160f2306
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2018-02-02 15:26:52 +0100

    add boards

--- model/src/Minesweeper.java
+++ model/src/Minesweeper.java
@@ -1,5 +1,24 @@
 public class Minesweeper {
-    
+    private RealBoard board;
 
+    public Minesweeper() {
+        board = new RealBoard(5);
+    }
 
+    public Board pick(int x, int y) {
+        if (board.isBomb(x, y)) {
+            return new LoserBoard(x,y);
+        }
+        board.turnUp(x,y);
+        return board.makeHiddenBoard();
+    }
+
+    public Board flag(int x, int y) {
+        board.setFlag(x, y);
+        return board.makeHiddenBoard();
+    }
+
+    public Board getInitialBoard(){
+        return board.makeHiddenBoard();
+    }
 }
