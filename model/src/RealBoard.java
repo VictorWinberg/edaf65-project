cfe418759fd502c7ac7e881b66d3a4a659c5efed
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2018-02-02 17:26:48 +0100

    point

--- model/src/RealBoard.java
+++ model/src/RealBoard.java
@@ -1,56 +1,62 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class RealBoard implements Board {
-
-    Square[][] field;
+    HashMap<Point, Square> field;
     int size;
     int bombs;
 
     public RealBoard(int size, int bombs) {
-        field = new Square[size][size];
+        field = new HashMap<>(size*size);
         this.size = size;
         this.bombs = bombs;
         fillAndAddBombs(bombs);
     }
 
     private void fillAndAddBombs(int bombs) {
-        int x, y;
+        Point p;
         while (bombs > 0) {
-            x = (int) (Math.random() * size);
-            y = (int) (Math.random() * size);
-            if (field[x][y] == null) {
-                field[x][y] = new Bomb();
+            p = new Point((int) (Math.random() * size), (int) (Math.random() * size));
+            if (field.get(p) == null) {
+                field.put(p, new Bomb());
                 bombs--;
             }
         }
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                field[i][j] = new Number(countNeighbourBombs(i, j));
+                field.put(new Point(i,j), new Number(countNeighbourBombs(i, j)));
             }
         }
     }
 
     private int countNeighbourBombs(int x, int y) {
-        List<Integer> neigbours = new ArrayList<>();
+        List<Point> neigbours = getNeighbourPoints();
+
 
         return 0;
     }
 
 
     @Override
-    public boolean isBomb(int x, int y) {
-        return (field[x][y].check() == -1);
+    public boolean isBomb(Point p) {
+        return (field.get(Point p).check() == -1);
     }
 
     @Override
-    public void setFlag(int x, int y) {
+    public void setFlag(Point p) {
 
     }
 
     @Override
-    public void pick(int x, int y) {
+    public void pick(Point p) {
         field[x][y].pick();
+        List<Point> neighbours = getNeighbourPoints(x,y);
+        for(Point nei : neighbours){
+            if(field[nei] == 0){
+
+            }
+        }
     }
 
     @Override
