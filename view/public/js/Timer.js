8c5bd55ccb958e09b9049b0b49f758f8035132d1
VictorWinberg <dat14vwi@student.lu.se>
2018-02-13 10:24:33 +0100

    add game gui view (copied) #7

---
+++ view/public/js/Timer.js
@@ -0,0 +1,35 @@
+function Timer() {
+  /* Create a timer to count the elapsed time in minutes, seconds */
+
+  this.count = function() {
+    /* Update the counter value when called */
+    var now = new Date();
+    var diff = now.getTime() - this.time.getTime();
+
+    var minutes = Math.floor(diff / 60000);
+    var seconds = ((diff % 60000) / 1000).toFixed(0);
+
+    this.counter = minutes + ":" + seconds;
+
+    if (seconds === 60) {
+      minutes += 1;
+      this.counter = ("0" + minutes).slice(-2) + ":00";
+    } else {
+      this.counter = ("0" + minutes).slice(-2) + ":" + (
+      "0" + seconds).slice(-2);
+    }
+  };
+
+  this.start = function(startTime) {
+    /* Set the timer start time and start counting */
+    var self = this;
+    this.time = startTime || new Date();
+    this.timer = setInterval(function() {
+      self.count();
+    }, 1000);
+  };
+
+  this.stop = function() {
+    clearInterval(this.timer);
+  }
+}
